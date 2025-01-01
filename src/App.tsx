d43f40b902e4f6d462ae751071f07dad9ffdc35d
Pertempto <pertemptodisce@gmail.com>
2025-01-01 10:06:18 -0500

    feat: improve theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import './App.css';
 
 export default function App() {
   return (<>
     <header>
+      <div id="title-wrapper">
+        <img src="/favicon.svg" alt="Logo of tree" width={32} height={32} />
+        <h1>
+          Kwila Demo
+        </h1>
+      </div>
       <SignedOut>
         <SignInButton />
       </SignedOut>
